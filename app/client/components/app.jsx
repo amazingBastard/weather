1c0c23475cfc4c861cb9b117844d7698eb5998b4
Damir <damir@withpulp.com>
2015-10-08 23:04:24 -0400

    app layout being rendered

---
+++ app/client/components/app.jsx
@@ -0,0 +1,17 @@
+App = React.createClass({
+    render() {
+        return (
+            <div className="app layout">
+                <header className="header">
+                    <h1>Weather Component</h1>
+                </header>
+                <main className="main">
+                    <p>Main Weather Widget</p>
+                </main>
+                <footer className="footer">
+                    <p>footer</p>
+                </footer>
+            </div>
+        );
+    }
+});
