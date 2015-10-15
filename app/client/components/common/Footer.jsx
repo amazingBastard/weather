a779520c5ad553538b3c87f460e8c8d2f9ecb1f4
Damir <damir@withpulp.com>
2015-10-15 14:59:03 -0400

    applied styles for header and footer modules

--- app/client/components/common/Footer.jsx
+++ app/client/components/common/Footer.jsx
@@ -2,7 +2,10 @@ Footer = React.createClass({
     render() {
         return (
             <footer className="animated fadeInUp footer module">
-                footer component
+                <p className="copyright">
+                    Project built by <a className="creator" href="http://damirvazgird.com" target="_blank">Damir Vazgird</a>.
+                    View source on <a className="source" href="https://github.com/amazingBastard/weather" target="_blank"> GitHub</a>.
+                </p>
             </footer>
         )
     }
--- app/client/components/common/Header.jsx
+++ app/client/components/common/Header.jsx
@@ -2,7 +2,7 @@ Header = React.createClass({
     render() {
         return (
             <header className="animated fadeInDown header module">
-                header component
+                <h1 className="logo">Weather Widget</h1>
             </header>
         )
     }
