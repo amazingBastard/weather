322bda99c8ec6a950b3bbeae4d0e58b1de096403
Damir <damir@withpulp.com>
2015-10-15 21:13:43 -0400

    working out styles for weather component

--- app/client/components/views/Weather.jsx
+++ app/client/components/views/Weather.jsx
@@ -18,17 +18,15 @@ Weather = React.createClass({
     },
 
     renderFigure() {
-        var icon = 'http://openweathermap.org/img/w/' + this.data.city.weather[0].icon + '.png';
         return (
-            <figure className="city" key={this.data.city._id}>
+            <figure className="city figure" key={this.data.city._id}>
                 <figcaption className="caption">
                     <h1 className="name">{this.data.city.name}</h1>
-                    <img className="icon" src={icon} />
-                    <p className="description">{this.data.city.weather[0].description}</p>
+                    <p className="description">[{this.data.city.weather[0].description}]</p>
                 </figcaption>
 
                 <div className="temp">
-                    <h2 className="current">{this.data.city.main.temp}ºC</h2>
+                    <h1 className="current">{this.data.city.main.temp}ºC</h1>
 
                     <p className="low">low: {this.data.city.main.temp_min}</p>
 
