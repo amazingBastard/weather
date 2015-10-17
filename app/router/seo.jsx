46f2bbddc48115ccf30cae88dd3157141e8e98dd
Damir <damir@withpulp.com>
2015-10-17 00:05:39 -0400

    added seo config

---
+++ app/router/seo.jsx
@@ -0,0 +1,12 @@
+SEO = new FlowRouterSEO();
+
+SEO.setDefaults({
+    title: 'City Weather | Reactive Weather for Your City',
+    description: 'A Weather Prototype using Meteor, React, and Openweather',
+    meta: {
+        'property="og:type"': 'web-app',
+        'property="og:site_name"': 'cityweather.meteor.com',
+        'name="twitter:card"': 'City Weather | Reactive Weather for Your City',
+        'name="twitter:site"': '@dvazgird'
+    }
+});
