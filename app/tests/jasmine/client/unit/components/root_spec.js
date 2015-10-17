d338d8e1d3d519645982cd453728532f6889a9de
Damir <damir@withpulp.com>
2015-10-17 18:08:01 -0400

    identified more component features in root spec

--- app/tests/jasmine/client/unit/components/root_spec.js
+++ app/tests/jasmine/client/unit/components/root_spec.js
@@ -4,4 +4,12 @@ describe('Root View Component', function() {
     it('should be mounted in DOM', function() {
         expect($el.length).toEqual(1);
     });
+
+    it('should get Meteor data from Openweather collection', function() {
+
+    });
+
+    it('should render options to choose different cities', function() {
+
+    });
 });
