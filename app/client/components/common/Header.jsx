153eaed5860960bdb1792d1cdd0a537c2e6eda7f
Damir <damir@withpulp.com>
2015-10-16 23:47:42 -0400

    title update

--- app/client/components/common/Header.jsx
+++ app/client/components/common/Header.jsx
@@ -3,7 +3,7 @@ Header = React.createClass({
         return (
             <header className="animated fadeInDown header module">
                 <a className="route" href="/">
-                    <h1 className="title">Weather <img className="logo" src="/images/logo.png"/> Widget</h1>
+                    <h1 className="title">City <img className="logo" src="/images/logo.png"/> Weather</h1>
                 </a>
             </header>
         )
