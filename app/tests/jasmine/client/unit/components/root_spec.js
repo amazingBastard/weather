cd744913b3fd3f6d86ad3cde9134826a5a743c7a
Damir <damir@withpulp.com>
2015-10-20 16:32:18 -0400

    some work on root spec

--- app/tests/jasmine/client/unit/components/root_spec.js
+++ app/tests/jasmine/client/unit/components/root_spec.js
@@ -10,10 +10,13 @@ describe('Root View Component', function () {
     it('should be mounted in DOM', function () {
         var root = <Root />;
         ReactTestUtils.renderIntoDocument(root);
+        expect(root.className).find('.root.view').toBeDefined();
     });
 
     it('should get Meteor Data from Openweather collection', function () {
+        var data = {};
 
+        expect(data).not.toBe(null);
     });
 
     it('should render options to choose different cities', function () {
