8b7cfeafa99903f66d66ede43bb8a25e9b7555f0
Damir <damir@withpulp.com>
2015-10-20 17:25:10 -0400

    added some tests for data in root_spec - wip

--- app/tests/jasmine/client/unit/components/root_spec.js
+++ app/tests/jasmine/client/unit/components/root_spec.js
@@ -1,22 +1,55 @@
 'use strict';
-var ReactTestUtils;
+var ReactTestUtils,
+    meteorData;
 
 describe('Root View Component', function () {
 
     beforeEach(function () {
         ReactTestUtils = React.addons.ReactTestUtils;
+        meteorData = {
+            cities: [
+                {
+                    name: 'New York'
+                },
+                {
+                    name: 'Chicago'
+                },
+                {
+                    name: 'Seattle'
+                },
+                {
+                    name: 'Houston'
+                },
+                {
+                    name: 'San Diego'
+                }
+            ]
+        };
     });
 
     it('should be mounted in DOM', function () {
-        var root = <Root />;
+        var root = '<Root />';
         ReactTestUtils.renderIntoDocument(root);
         expect(root.className).find('.root.view').toBeDefined();
     });
 
     it('should get Meteor Data from Openweather collection', function () {
-        var data = {};
-
-        expect(data).not.toBe(null);
+        // @TODO: loop this
+        expect(meteorData.cities).toEqual(jasmine.objectContaining({
+            name: 'New York'
+        }));
+        expect(meteorData.cities).toEqual(jasmine.objectContaining({
+            name: 'Chicago'
+        }));
+        expect(meteorData.cities).toEqual(jasmine.objectContaining({
+            name: 'Seattle'
+        }));
+        expect(meteorData.cities).toEqual(jasmine.objectContaining({
+            name: 'Houston'
+        }));
+        expect(meteorData.cities).toEqual(jasmine.objectContaining({
+            name: 'San Diego'
+        }));
     });
 
     it('should render options to choose different cities', function () {
