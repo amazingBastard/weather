e75fbb462884cc166268902b7a90df1e9f5d2fb4
Damir <damir@withpulp.com>
2015-10-25 19:09:55 -0400

    init weather spec

---
+++ app/tests/jasmine/client/unit/components/weather_spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('Weather View Component', function () {
+
+    it('should be mounted in DOM', function () {
+
+    });
+
+    it('should show the temperature for the chosen city', function () {
+
+    });
+});
